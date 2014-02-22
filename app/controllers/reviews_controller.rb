a8cbd740f049abe67875ef065d61fb6d29da695d
Patrick Perey <the4dpatrick@yahoo.com>
2014-02-21 19:59:48 -0800

    Reviews update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -38,6 +38,11 @@ class ReviewsController < ApplicationController
   end
 
   def update
+    if @review.update(review_params)
+      redirect_to @review, notice: 'Review was successfully updated'
+    else
+      redirect_to :edit
+    end
   end
 
   private
