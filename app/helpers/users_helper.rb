0adee0c406e63523d45c951521504b0ac9266069
Patrick Perey <the4dpatrick@yahoo.com>
2014-02-22 23:58:31 -0800

    Cars/new active class_name

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 module UsersHelper
   def sidebar_active_link_to(link_path)
-    class_name = current_page?(link_path) && request.fullpath == link_path ? 'active' : nil
+    if current_page?(link_path) || request.fullpath[0..(link_path.size - 1)] == link_path
+      class_name = 'active'
+    else
+      class_name = nil
+    end
 
     content_tag(:li, class: class_name) do
       link_to(link_path) do
