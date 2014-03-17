0d4686891cf601bdf8f750af0bff788d5e885a35
Patrick Perey <the4dpatrick@yahoo.com>
2014-03-17 00:16:23 -0700

    Truncate review body

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def truncate_review(review)
+    output = truncate(review.body, length: 145)
+    output += link_to('(more)', review_path(review)) if review.body.size > 144
+    output.html_safe
+  end
 end
