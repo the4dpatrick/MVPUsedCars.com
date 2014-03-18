d3d053fa7fa3e33227e4487b12ee2c46ac3adb3f
Patrick Perey <the4dpatrick@yahoo.com>
2014-03-17 21:28:11 -0700

    Lower truncate character count

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,7 @@
 module PagesHelper
   def truncate_review(review)
-    output = truncate(review.body, length: 145)
-    output += link_to('(more)', review_path(review)) if review.body.size > 144
+    output = truncate(review.body, length: 140)
+    output += link_to('(more)', review_path(review)) if review.body.size > 140
     output.html_safe
   end
 end
